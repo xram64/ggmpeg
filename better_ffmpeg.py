23d8840448a42a14c8a6fcd37bc12a456362de69
xram64 <33797815+xram64@users.noreply.github.com>
2022-10-15 01:43:41 -0400

    Changes and refactoring
    Last commit before rename to 'ggmpeg'

--- better_ffmpeg.py
+++ better_ffmpeg.py
@@ -1,33 +1,46 @@
 #!/bin/python3
-############################
-##|  better_ffmpeg v0.1  |##
-############################
-##|    xram - 10/8/22    |##
-############################
-# Ref:
-#  - https://github.com/kkroening/ffmpeg-python
-#  - https://kkroening.github.io/ffmpeg-python
+
+########################
+##|   ggmpeg  v0.1   |##
+########################
+##|       xram       |##
+########################
+
+# [References]
+# - https://github.com/kkroening/ffmpeg-python
+# - https://kkroening.github.io/ffmpeg-python
+# - https://ffmpeg.org/ffmpeg-formats.html#concat-1
+# - https://ffmpeg.org/ffmpeg-filters.html#drawtext-1
+# - https://ffmpeg.org/ffmpeg-filters.html#subtitles
+# - https://ffmpeg.org/ffmpeg-utils.html#Expression-Evaluation
+# - http://ffmpeg.org/pipermail/ffmpeg-user/2021-January/051566.html
 
 from os import path
-from posixpath import abspath
+from ctypes.wintypes import MAX_PATH
+from tkinter.tix import MAX
 from typing import Dict, List, Tuple
-from enum import Enum, auto
+from enum import Enum
 from glob import glob
-from xmlrpc.client import Boolean
 import ffmpeg
+from ffmpeg._run import Error
 
-# Basic FPS and interpolation options
-class Opt(Enum):
+# General options
+class GeneralOpt(Enum):
     input_fps = 'input_fps'
-    interp_fps = 'interp_fps'
+    
+# Interpolation filter options
+class IntrpOpt(Enum):
+    fps = 'fps'
     mi_mode = 'mi_mode'
     mc_mode = 'mc_mode'
     vsbmc = 'vsbmc'
     me = 'me'
     mb_size = 'mb_size'
+    
+    MODE_OPTS_BLEND = [fps, mi_mode]
+    MODE_OPTS_MCI = [fps, mi_mode, mc_mode, vsbmc, me, mb_size]
 
-# Options for text overlays
-# Ref: https://ffmpeg.org/ffmpeg-filters.html#drawtext-1
+# Text overlay filter options
 class TextOpt(Enum):
     text = 'text'                       # text to render
     expansion = 'expansion'             # text mode: 'normal' (default), 'none' (plaintext)
@@ -37,15 +50,15 @@ class TextOpt(Enum):
     font = 'font'                       # valid options: 'Sans'
     font_file = 'fontfile'              # path to a font file
     font_size = 'fontsize'              # default 16
-    font_color = 'fontcolor'            # includes opacity (e.g. 'white@0.8')
+    font_color = 'fontcolor'            # includes opacity (e.g. 'white@0.8' or '#ffffff@0.8')
 
     box_enabled = 'box'                 # 1=enable, 0=disable
     box_color = 'boxcolor'              # includes opacity
     box_border_width = 'boxborderw'     # padding for box around text, in pixels
 
-    shadow_color = 'shadowcolor'
-    shadow_x = 'shadowx'
-    shadow_y = 'shadowy'
+    shadow_color = 'shadowcolor'        # includes opacity
+    shadow_x = 'shadowx'                # offset for text shadow (default 0)
+    shadow_y = 'shadowy'                # offset for text shadow (default 0)
 
 
 def get_files(input_dir, input_pattern):
@@ -58,22 +71,21 @@ def get_files(input_dir, input_pattern):
     
     return (input_path_glob, input_fullpaths)
 
+
 def make_video(input_dir: str,
                input_pattern: str,
                output_filename: str,
                options: Dict[str, str|int|float],
                overlay_text: Dict[str, str] = {},
                final_frame_dur: int = 1,
-               debug: Boolean = False
+               debug: bool = False
                ):
     # Other options:
     #  - loop=1 : "loop over input" [.input()]
-    #  - mb_size=16 : "macroblock size, default=16" [.filter()]
     #  - search_param=32 : "motion est. search parameter, default=32" [.filter()]
 
-    # TODO: Allow decimal/rational inputs for Opt.interp_fps (allow input in decimal, then convert to fraction before passing to ffmpeg).
-    # TODO: Implement 'last_frame_duration' option to hold/repeat last image for a certain amount of time.
-    #         Alternatively, allow for providing a sequence of numbers to specify the duration of each frame.
+    # TODO: Allow decimal/rational inputs for IntrpOpt.fps (allow input in decimal, then convert to fraction before passing to ffmpeg).
+    # TODO: Allow for providing a sequence of numbers to specify the duration of each frame (or other properties)?
     # TODO: Allow for non-interactive command-line arg entry.
     # TODO: Add a keybind to skip all remaining prompts and use defaults.
     # TODO: Delete text file after generation (optionally?).
@@ -82,18 +94,16 @@ def make_video(input_dir: str,
 
     tmp_concat_file_name = "~ffmpeg_inputs.txt"
 
-    frame_duration = round(1/options[Opt.input_fps], 8)  # frame_duration = seconds per frame
+    frame_duration = round(1/options[GeneralOpt.input_fps], 8)  # frame_duration = "seconds per frame"
     
     input_path_glob, input_fullpaths = get_files(input_dir, input_pattern)
     
-
     print(f"Found {len(input_fullpaths)} files matching {input_path_glob}.")
 
     # Fail if no files were found
     if len(input_fullpaths) < 1:
-        print(input_path_glob)
+        print(f"[Error] Found no files matching '{input_path_glob}'")
         return  # TODO: Return error
-
 
     with open(tmp_concat_file_name, "wb") as tmp_concat_file:
         tmp_concat_file.write(f"## Input file for ffmpeg 'concat' format. Total frames: {len(input_fullpaths)}. Total duration: {len(input_fullpaths)*frame_duration} s.\n\n".encode())
@@ -111,55 +121,74 @@ def make_video(input_dir: str,
                 tmp_concat_file.write(f"file '{input_fullpaths[-1]}'\n".encode())
                 tmp_concat_file.write(f"duration {frame_duration}\n".encode())
 
-    # Set empty output filename to first input image name
+    # Set default output path (matching first input image filename), and set absolute path for a provided filename (inside input folder)
     if output_filename == "":
-        output_filename = path.abspath(input_fullpaths[0]).removesuffix('.png') + '.mp4'
+        output_filepath = path.abspath(input_fullpaths[0]).removesuffix('.png') + '.mp4'
+        # Catch paths that are too long
+        if len(output_filepath) > MAX_PATH:
+            path_dir = path.dirname(input_fullpaths[0])
+            used_chars = len(path_dir) + len('//') + len('.mp4')
+            if used_chars >= MAX_PATH:
+                print(f"[Error] Output file destination folder too long.")
+                return  # TODO: Return error
+            else:
+                # If enough space is left after the dir, use truncated filename
+                path_base = path.basename(input_fullpaths[0]).removesuffix('.png')
+                output_filepath = path.join(path_dir, path_base[:(MAX_PATH - used_chars)] + '.mp4')
+    else:
+        output_filepath = path.join(path.dirname(input_fullpaths[0]), output_filename)
+        # Catch paths that are too long
+        if len(output_filepath) > MAX_PATH:
+            print(f"[Error] Output filepath too long.")
+            return  # TODO: Return error
 
+    #>> Configure input, initialize `stream`
     stream = ffmpeg.input(tmp_concat_file_name,
                           format='concat',
                           safe=0)
+    
+    #>> Apply interpolation filter
+    options_kwargs = {}
+        
+    if options[IntrpOpt.mi_mode] == 'blend':
+        # Prepare keyword args for filter options
+        for arg in options.keys():
+            if arg.value in IntrpOpt.MODE_OPTS_BLEND.value:
+                options_kwargs[arg.value] = options[arg]
+                
+        stream = ffmpeg.filter(stream, 'minterpolate', **options_kwargs)
+        
+    elif options[IntrpOpt.mi_mode] == 'mci':
+        # Prepare keyword args for filter options
+        for arg in options.keys():
+            if arg.value in IntrpOpt.MODE_OPTS_MCI.value:
+                options_kwargs[arg.value] = options[arg]
+                
+        stream = ffmpeg.filter(stream, 'minterpolate', **options_kwargs)
 
-    if options[Opt.mi_mode] == 'blend':
-        stream = ffmpeg.filter(stream,
-                               'minterpolate',
-                               fps=options[Opt.interp_fps],
-                               mi_mode=options[Opt.mi_mode])
+    #>> Apply text overlay filter
+    if overlay_text.get(TextOpt.text) is not None:
+        overlay_text_kwargs = {}
+        # Prepare keyword args for text overlay
+        for param in overlay_text.keys():
+            overlay_text_kwargs[param.value] = overlay_text[param]
+        
+        stream = ffmpeg.drawtext(stream, escape_text=False, **overlay_text_kwargs)
 
-    elif options[Opt.mi_mode] == 'mci':
-        stream = ffmpeg.filter(stream,
-                               'minterpolate',
-                               fps=options[Opt.interp_fps],
-                               mi_mode=options[Opt.mi_mode],
-                               mc_mode=options[Opt.mc_mode],
-                               vsbmc=options[Opt.vsbmc],
-                               me=options[Opt.me],
-                               mb_size=options[Opt.mb_size])
+    #>> Configure output
+    stream = ffmpeg.output(stream, output_filepath, format='mp4', vcodec='libx264', crf=15)
 
-
-    ## WIP ##########################################################
-    if len(overlay_text) > 0:
-        stream = ffmpeg.drawtext(stream, escape_text=False,
-            text=overlay_text[TextOpt.text],
-            x=overlay_text[TextOpt.pos_x],
-            y=overlay_text[TextOpt.pos_y],
-            fontfile=overlay_text[TextOpt.font_file],
-            fontsize=overlay_text[TextOpt.font_size],
-            fontcolor=overlay_text[TextOpt.font_color],
-            box=overlay_text[TextOpt.box_enabled],
-            boxcolor=overlay_text[TextOpt.box_color],
-            boxborderw=overlay_text[TextOpt.box_border_width])
-    #################################################################
-
-
-    stream = ffmpeg.output(stream, output_filename, format='mp4', vcodec='libx264', crf=15)
-
-    out, err = ffmpeg.run(stream, overwrite_output=True, capture_stdout=True, capture_stderr=True)
+    #>> Run ffmpeg
+    try:
+        out, err = ffmpeg.run(stream, overwrite_output=True, capture_stdout=True, capture_stderr=True)
+    except Error as e:
+        print("-------- Error thrown by `ffmpeg._run` --------")
+        print(f"\n[stdout]\n{e.stdout}")
+        print(f"\n[stderr]\n{e.stderr}")
     
     if debug:
-        print('\n[Output]')
-        print(out)
-        print('\n[Error]')
-        print(err)
+        print(f"\n[Output]\n{out}")
+        print(f"\n[Error]\n{err}")
 
 
 def parse_choice(choice: str, options: List[str]|Tuple[int,int]|Tuple[float,float], default: str):
@@ -210,23 +239,23 @@ def parse_choice(choice: str, options: List[str]|Tuple[int,int]|Tuple[float,floa
 
 if __name__ == '__main__':
     filter_options = {
-        Opt.mc_mode: ['aobmc', 'obmc'],
-        Opt.vsbmc: ['0', '1'],
-        Opt.me: ['ds', 'epzs', 'esa', 'fss', 'hexbs', 'ntss', 'tdls', 'tss', 'umh'],
-        Opt.mb_size: (1,512),
+        IntrpOpt.mc_mode: ['aobmc', 'obmc'],
+        IntrpOpt.vsbmc: ['0', '1'],
+        IntrpOpt.me: ['ds', 'epzs', 'esa', 'fss', 'hexbs', 'ntss', 'tdls', 'tss', 'umh'],
+        IntrpOpt.mb_size: (1,512),
     }
     filter_defaults = {
-        Opt.mc_mode: 'aobmc',
-        Opt.vsbmc: '1',
-        Opt.me: 'epzs',
-        Opt.mb_size: 16,
+        IntrpOpt.mc_mode: 'aobmc',
+        IntrpOpt.vsbmc: '1',
+        IntrpOpt.me: 'epzs',
+        IntrpOpt.mb_size: 16,
     }
     docs = {
-        'Notes': ['`*` indicates defaults.', '`mc_mode`, `vsbmc`, `me`, `mb_size`, and `search_param` only apply to `mi_mode=mci`.']
+        'Notes': ['`*` indicates defaults.', '`mc_mode`, `vsbmc`, `me`, and `mb_size` only apply to `mi_mode=mci`.']
     }
 
-    print('better_ffmpeg')
-    print('Defaults indicated by ^')
+    print('|==| better_ffmpeg |==|')
+    print('| - Defaults indicated by ^')
     print()
 
 
@@ -250,16 +279,16 @@ if __name__ == '__main__':
             output_filename += ".mp4"
 
         c_in = input('Input FPS [^15]: ')
-        choices[Opt.input_fps] = parse_choice(c_in, (0.1, 144.0), 15.0)
+        choices[GeneralOpt.input_fps] = parse_choice(c_in, (0.1, 144.0), 15.0)
 
         c_in = input('Interpolation FPS [^60]: ')
-        choices[Opt.interp_fps] = parse_choice(c_in, (1, 144), 60)
+        choices[IntrpOpt.fps] = parse_choice(c_in, (1, 144), 60)
 
-        c_in = input(f'{Opt.mi_mode.value} [^mci/blend]: ')
-        choices[Opt.mi_mode] = parse_choice(c_in, ['blend', 'mci'], 'mci')
+        c_in = input(f'{IntrpOpt.mi_mode.value} [^mci/blend]: ')
+        choices[IntrpOpt.mi_mode] = parse_choice(c_in, ['blend', 'mci'], 'mci')
 
         # Continue collecting 'mci' filter options, if selected
-        if choices[Opt.mi_mode] == 'mci':
+        if choices[IntrpOpt.mi_mode] == 'mci':
             for option in filter_options.keys():
                 # Make prompt string
                 pmt_start = f'{option.value} ['
